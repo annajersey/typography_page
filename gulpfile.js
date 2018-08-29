8a9cf34af60509b68db246519b0c288cd74843f6
anna <anna.bogomiagkova@codeit.com.ua>
2018-08-29 15:31:36 +0300

    cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,38 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var sass = require('gulp-sass');
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('css', function() {
+gulp.task('css', function () {
     return gulp.src([
         'node_modules/font-awesome/css/font-awesome.min.css'])
         .pipe(gulp.dest('src/css/'));
 });
-gulp.task('fonts', function() {
+gulp.task('fonts', function () {
     return gulp.src([
         'node_modules/font-awesome/fonts/fontawesome-webfont.*'])
         .pipe(gulp.dest('src/fonts/'));
 });
 // Move the javascript files into our /src/js folder
-gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js'])
+gulp.task('js', function () {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js'])
         .pipe(gulp.dest("src/js"))
         .pipe(browserSync.stream());
 });
-// gulp.task('js', function() {
-//     gulp.src([
-//         'node_modules/jquery/dist/jquery.js',
-//         'node_modules/bootstrap/dist/js/bootstrap.js',
-//         'src/js/**/*.js'
-//     ])
-//         .pipe(uglify())
-//         .pipe(concat('script.js'))
-//         .pipe(gulp.dest('web/dist/js'));
-// });
-//Static Server + watching scss/html files
-gulp.task('serve', ['sass','fonts','css'], function() {
-
+gulp.task('serve', ['sass', 'fonts', 'css'], function () {
     browserSync.init({
         server: "./src",
         port: 4545
     });
-
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('default', ['js','serve']);
+gulp.task('default', ['js', 'serve']);
