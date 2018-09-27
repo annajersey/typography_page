4818c1a633db68ad19379f9c9a9ab1843ef26e99
anna <anna.bogomiagkova@codeit.com.ua>
2018-09-27 17:52:16 +0300

    moving to webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import bootstrap from './../node_modules/bootstrap/dist/css/bootstrap.css';
+import awesome from "./../node_modules/font-awesome/css/font-awesome.min.css";
+import style from "./scss/style.scss";
+import jQuery from "./../node_modules/jquery/dist/jquery.js";
+import bootstrapScript from "./../node_modules/bootstrap/dist/js/bootstrap.js";
+import script from "./js/dropdowns.js";
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [{loader: "html-loader", options: {minimize: false}}]
+            },
+            {
+                test: /\.(png|jpe?g)/i,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            name: "./img/[name].[ext]",
+                            limit: 10000
+                        }
+                    },
+                    {
+                        loader: "img-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
+            },
+            {
+                test: /\.js$/,
+                include: [
+                    path.resolve(__dirname, "js")
+                ],
+                exclude: /node_modules/,
+                use: {
+                    loader: "file-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "src/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default'],
+        })
+    ]
+};
